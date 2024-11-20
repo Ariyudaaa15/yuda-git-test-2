8ef0bfed7d0a799f1b2f69b7e40fc461765bfec4
Ariyudaaa15 <mankariyuda15@gmail.com>
2024-11-20 12:58:16 +0800

    menambahkan list baru

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 
     <h1>Hello World!!!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium at similique aliquid doloribus atque exercitationem aspernatur. Asperiores fugiat deleniti, optio impedit doloribus atque recusandae laudantium eum eos dignissimos sunt dolor amet expedita libero fuga natus ea unde incidunt corrupti ut veniam et, necessitatibus accusamus laboriosam? Sit blanditiis libero similique fugit!</p>
+
+    <ul>
+        <li>test 1</li>
+        <li>test 2</li>
+        <li>test 3</li>
+    </ul>
 </body>
 </html>
